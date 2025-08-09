f206814e242323beaf902a751ab6db8b5024972a
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-09 17:50:01 +0100

    Added the constants....awaiting deployment of contract

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,3 +2,11 @@ import { useState, useEffect } from 'react'
 import { ethers } from 'ethers'
 import TaskItem from '../components/TaskItem'
 import contractData from '../lib/TodoList.json'
+
+export default function Home() {
+    const [tasks, setTasks] = useState([])
+    const [input, setInput] = useState('')
+    const [account, setAccount] = useState('')
+
+    const contractAddress = 'YOUR_DEPLOYED_CONTRACT_ADDRESS'
+}
